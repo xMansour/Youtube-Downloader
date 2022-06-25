752d72d8332715c38977da477a158a8095ae4a90
xMansour <MEMansour96@gmail.com>
2022-06-25 23:16:26 +0200

    Video downloading is working. TODO::playlists

--- src/main.js
+++ src/main.js
@@ -1,24 +1,41 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, ipcMain, Notification } = require("electron");
+const path = require("path");
 const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-        },
-    })
+  const win = new BrowserWindow({
+    width: 600,
+    height: 420,
+    icon: path.join(__dirname, "icons", "launcher_icon2.jpg"),
+    webPreferences: {
+      nodeIntegration: true, //gives access to the nodeJS api
+      contextIsolation: false,
+      enableRemoteModule: true,
+      //devTools: false,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
-    win.loadFile(path.join(__dirname, 'index.html'))
-}
+  win.loadFile(path.join(__dirname, "index.html"));
+  //win.setResizable(false);
+  win.setMenuBarVisibility(false);
+};
 
 app.whenReady().then(() => {
-    createWindow()
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-})
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+ipcMain.handle("show-notification", (event, ...args) => {
+  const notification = {
+    title: "Video Downloaded",
+    body: `${args[0]}`,
+    icon: path.join(__dirname, "icons", "launcher_icon2.jpg"),
+  };
+
+  new Notification(notification).show();
+});
--- src/preload.js
+++ src/preload.js
@@ -1,53 +1,130 @@
-window.addEventListener('DOMContentLoaded', () => {
-    /*const YoutubeDlWrap = require("youtube-dl-wrap");
+window.addEventListener("DOMContentLoaded", () => {
+    const fs = require("fs");
+    const ytdl = require("ytdl-core");
+    const { ipcRenderer } = require("electron");
     const path = require("path");
-    const youtubeDlWrap = new YoutubeDlWrap(path.join(process.cwd(), 'resources', 'api', 'youtube-dl.exe'));
-    youtubeDlEventEmitter = youtubeDlWrap.exec(["https://www.youtube.com/watch?v=aqz-KE-bpKQ",
-        "-f", "best", "-o", "output.mp4"])
-        .on("progress", (progress) =>
-            console.log(progress.percent, progress.totalSize, progress.currentSpeed, progress.eta))
-        .on("youtubeDlEvent", (eventType, eventData) => console.log(eventType, eventData))
-        .on("error", (error) => console.error(error))
-        .on("close", () => console.log("all done"));
+    const videoUrl = document.querySelector("#videoUrl");
+    const formatsTable = document.querySelector("#formatTable");
+    const formatsTableBody = document.querySelector("#formatListBody");
+    const btnSearch = document.querySelector("#btnSearch");
+    const loadingDiv = document.querySelector("#loadingDiv");
+    const btnSearchText = document.querySelector("#btnSearchText");
+    const progressBar = document.querySelector("#progressBar");
+    const progressBarContainer = document.querySelector("#progressBarContainer");
+    const progressLabel = document.querySelector("#progressLabel");
+    const playlistCheckBox = document.querySelector("#playlistCheckBox");
+    const playlistNumbering = document.querySelector("#playlistNumbering");
 
-    console.log(youtubeDlEventEmitter.youtubeDlProcess.pid);*/
+    const ytpl = require("ytpl");
 
-    const fs = require('fs')
-    const ytdl = require('ytdl-core')
-    const videoUrl = document.querySelector('#videoUrl')
-    const formatList = document.querySelector('#formatList')
-    const formatTable = document.querySelector('#formatTable')
-    const formatListBody = document.querySelector('#formatListBody')
-    const btnSearch = document.querySelector('#btnSearch')
-    btnSearch.addEventListener('click', (event) => {
-        //formatList
-        ytdl.getInfo(videoUrl.value).then((data) => {
-            console.log(data)
+    playlistCheckBox.addEventListener("click", (event) => {
+        playlistNumbering.toggleAttribute("disabled");
+    });
+
+    btnSearch.addEventListener("click", async (event) => {
+        if (playlistCheckBox.checked) {
+            const search = await ytpl(videoUrl.value);
+            console.log(search);
+            Object.keys(search.items).forEach((key) => {
+                console.log(search.items[key].url);
+            });
+        }
+
+        btnSearchText.classList.toggle("visually-hidden");
+        loadingDiv.classList.toggle("visually-hidden");
+        btnSearch.disabled = true;
+        await ytdl.getInfo(videoUrl.value).then((data) => {
+            btnSearchText.classList.toggle("visually-hidden");
+            loadingDiv.classList.toggle("visually-hidden");
+            //formatsTable.classList.remove("visually-hidden");
+            formatsTableBody.innerHTML = "";
+            btnSearch.disabled = false;
+            let counter = 1;
             Object.keys(data.formats).forEach((key) => {
-                //TODO:: add items to the table
-                const node = document.createElement('tr')
                 if (data.formats[key].hasAudio && data.formats[key].hasVideo) {
-                    const textnode = document.createTextNode(
-                        data.formats[key].qualityLabel +
-                            '     ' +
-                            data.formats[key].fps +
-                            ' fps     ' +
-                            (
-                                data.formats[key].approxDurationMs /
-                                (1000 * 60)
-                            ).toFixed(2) +
-                            ' Mins     ' +
-                            data.formats[key].hasAudio +
-                            '     ' +
-                            data.formats[key].hasVideo
-                    )
-                    node.appendChild(textnode)
-                    formatList.appendChild(node)
+                    const tr = formatsTableBody.insertRow();
+
+                    const idLabel = tr.insertCell();
+                    idLabel.innerText = counter++;
+
+                    const qualityLabel = tr.insertCell();
+                    qualityLabel.innerText = data.formats[key].qualityLabel;
+
+                    /*const durationLabel = tr.insertCell();
+                              durationLabel.innerText =
+                                  (data.formats[key].approxDurationMs / (1000 * 60)).toFixed(2) +
+                                  " Mins";
+          
+                              const tagLabel = tr.insertCell();
+                              tagLabel.innerText = data.formats[key].itag;*/
+
+                    const downloadLabel = tr.insertCell();
+                    const btnDownload = document.createElement("button");
+                    btnDownload.type = "button";
+                    btnDownload.innerText = "Download";
+                    btnDownload.classList.add("btn", "btn-primary");
+                    downloadLabel.appendChild(btnDownload);
+                    //console.log(data);
+                    //console.log(data.formats[key]);
+                    //console.log(data.formats[key].contentLength);
+
+                    btnDownload.addEventListener("click", (event) => {
+                        btnDownload.innerText = "Loading...";
+                        btnDownload.disabled = true;
+                        const fileName =
+                            data.videoDetails.title +
+                            " " +
+                            data.formats[key].qualityLabel +
+                            ".mp4";
+                        /*let starttime;
+                                                ytdl(videoUrl.value, data.formats[key].itag)
+                                                    .pipe(fs.createWriteStream(data.videoDetails.title))
+                                                    .once("response", () => {
+                                                        starttime = Date.now();
+                                                    })
+                                                    .on('progress', (chunkLength, downloaded, total) => {
+                                                        const percent = downloaded / total;
+                                                        const downloadedMinutes = (Date.now() - starttime) / 1000 / 60;
+                                                        const estimatedDownloadTime = (downloadedMinutes / percent) - downloadedMinutes;
+                        
+                                                        console.log(percent);
+                        
+                                                        console.log(estimatedDownloadTime);
+                                                    })*/
+
+                        const video = ytdl(videoUrl.value, {
+                            filter: function (format) {
+                                return format.itag === Number(data.formats[key].itag);
+                            },
+                        });
+                        let starttime;
+                        video.pipe(
+                            fs.createWriteStream(
+                                path.join(process.cwd(), path.join("downloads", fileName))
+                            )
+                        );
+                        video.once("response", () => {
+                            starttime = Date.now();
+                            btnDownload.innerText = "Downloading...";
+                        });
+                        video.on("progress", (chunkLength, downloaded, total) => {
+                            const percent = downloaded / total;
+                            const downloadedMinutes = (Date.now() - starttime) / 1000;
+                            const estimatedDownloadTime =
+                                downloadedMinutes / percent - downloadedMinutes;
+                            progressBarContainer.classList.remove("visually-hidden");
+                            progressBar.style.width = percent.toFixed(2) * 100 + "%";
+                            progressBar.innerText = percent.toFixed(2) * 100 + "%";
+                        });
+                        video.on("end", () => {
+                            progressBar.classList.add("bg-success");
+                            btnDownload.innerText = "Downloaded";
+                            btnDownload.disabled = true;
+                            ipcRenderer.invoke("show-notification", fileName);
+                        });
+                    });
                 }
-            })
-        })
-    })
-    /*ytdl.getInfo('http://www.youtube.com/watch?v=aqz-KE-bpKQ').then((data) => {
-        console.log(data.formats);
-    })*/
-})
+            });
+        });
+    });
+});
